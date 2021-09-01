e9efc8be9ded1f53e517df0f9c1589e8c10d267f
BodoBence <cyke216@gmail.com>
2021-09-01 23:01:18 +0200

    main Squid animation added (html, css) to /animation_squid route.

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/animation_squid', methods=['POST', 'GET'])
+def animation_squid():
+    return render_template('animation_squid.html')
+
 if __name__ == '__main__':
     app.run(debug=True)

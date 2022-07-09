b6588024e510ed5c1ca30c8d245353595ff47259
Fruzsi Zsolyomi <zsfruzsi@gmail.com>
2022-07-09 19:46:09 +0200

    LS-5 Implement ReductionPlannerService and MixCalculatorService.

---
+++ venv/lib/liquidswirl/services/mix_calculator_service.py
@@ -0,0 +1,19 @@
+class MixCalculatorService:
+    def __init__(self, base_ml, base_nicotine_cc, nicotine_ml, nicotine_premix_cc):
+        self.base_ml = base_ml
+        self.base_nicotine_cc = base_nicotine_cc
+        self.nicotine_ml = nicotine_ml
+        self.nicotine_premix_cc = nicotine_premix_cc
+    
+    def get_base_ml(self):
+        pass
+
+    def get_nicotine_cc(self):
+        pass
+
+    def get_nicotine_ml(self):
+        pass
+
+    def get_nicotine_mg(self):
+        pass
+
